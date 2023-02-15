0d54f5e6c0eb201344eed43dfb2b0e32a5760173
Pengcheng Cao <94662961+pccao0912@users.noreply.github.com>
2023-02-15 15:14:51 -0800

    Error message

--- libuthread/uthread.c
+++ libuthread/uthread.c
@@ -11,7 +11,6 @@
 #include "queue.h"
 
 struct uthread_tcb {
-	/* TODO Phase 2 */
 	void *sp;	//  stack pointer
 	int state;	//  running(0), ready(1) ,block(2), unblock(3), or exited(4)
 	uthread_ctx_t *context;//  set of registers
@@ -57,7 +56,7 @@ void uthread_exit(void)
 	struct uthread_tcb *temp = uthread_current();
 	struct uthread_tcb *next = malloc(sizeof(struct uthread_tcb));
 	queue_dequeue(readyqueue, (void**)&next);
-	temp->state = 0;
+	temp->state = 4;
 	uthread_ctx_switch(temp->context,next->context);
 	uthread_ctx_destroy_stack(temp->sp);
 	free(temp);
@@ -68,10 +67,15 @@ int uthread_create(uthread_func_t func, void *arg)
 	struct uthread_tcb *temp_tcb = malloc(sizeof(struct uthread_tcb));
 	uthread_ctx_t *new_context = malloc(sizeof(uthread_ctx_t));
 	void* sp = uthread_ctx_alloc_stack();
+	if (temp_tcb == NULL || new_context == NULL || sp == NULL) {
+		return -1;
+	}
 	temp_tcb->state = 1;
 	temp_tcb->sp = sp;
 	temp_tcb->context = new_context;
-	uthread_ctx_init(new_context,sp,func,arg);
+	if (uthread_ctx_init(new_context,sp,func,arg)) {
+		return -1;
+	}
 	queue_enqueue(readyqueue, temp_tcb);
 	return 0;
 }
@@ -81,17 +85,21 @@ int uthread_run(bool preempt, uthread_func_t func, void *arg)
 
 
 	readyqueue = queue_create();
-	//current_thread = &main_thread;
-	if(preempt) {
+	if (preempt) {
 		printf("testing: preemptive scheduling is enabled.\n");
 	}
 	//  main thread initialization?
 	struct uthread_tcb* main_thread = (struct uthread_tcb*)malloc(sizeof(struct uthread_tcb));
-  main_thread->context = (uthread_ctx_t*)malloc(sizeof(uthread_ctx_t));
-  main_thread->sp = uthread_ctx_alloc_stack();
+	main_thread->context = (uthread_ctx_t*)malloc(sizeof(uthread_ctx_t));
+	main_thread->sp = uthread_ctx_alloc_stack();
+	if (main_thread == NULL || main_thread->context == NULL) {
+		return -1;
+	}
 	current_thread = main_thread;
 
-	uthread_create(func, arg);
+	if (uthread_create(func, arg)) {
+		return -1;
+	}
 
 	while(1) {
 		if(!queue_length(readyqueue)) {
@@ -99,7 +107,7 @@ int uthread_run(bool preempt, uthread_func_t func, void *arg)
 		}
 		uthread_yield();
 	}
-	//queue_destroy(readyqueue);
+	queue_destroy(readyqueue);
 	return 0;
 }
 
