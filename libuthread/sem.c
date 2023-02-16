bce09e8ae6161f6ebc75a12e398a0b954508cf0c
Pengcheng Cao <94662961+pccao0912@users.noreply.github.com>
2023-02-15 21:55:49 -0800

    Update sem.c

--- libuthread/sem.c
+++ libuthread/sem.c
@@ -33,13 +33,10 @@ int sem_down(sem_t sem)
 	if (sem == NULL) {
 		return -1;
 	}
-	if (sem->count > 0) {
-		sem->count--;
-	} else {
-		struct uthread_tcb *temp = uthread_current();
-		queue_enqueue(sem->waiting_queue, temp);
-		uthread_block();
-	}
+	struct uthread_tcb *temp = uthread_current();
+	queue_enqueue(sem->waiting_queue, temp);
+	uthread_block();
+	sem->count--;
 	return 0;
 }
 
@@ -48,12 +45,11 @@ int sem_up(sem_t sem)
 	if (sem == NULL) {
 		return -1;
 	}
-	struct uthread_tcb *next;
+	struct uthread_tcb *temp = uthread_current();
 	sem->count++;
 	if (queue_length(sem->waiting_queue) != 0) {
-		queue_dequeue(sem->waiting_queue, (void**)&next);
-		uthread_unblock(next);
+		queue_dequeue(sem->waiting_queue, (void**)&temp);
+		uthread_unblock(temp);
 	}
 	return 0;
 }
-
