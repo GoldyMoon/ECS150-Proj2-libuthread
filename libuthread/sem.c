2e7e965a1d90e106d18a494ffd458dfef3ba008c
Pengcheng Cao <94662961+pccao0912@users.noreply.github.com>
2023-02-15 21:41:14 -0800

    sem_down debug

--- libuthread/sem.c
+++ libuthread/sem.c
@@ -33,10 +33,13 @@ int sem_down(sem_t sem)
 	if (sem == NULL) {
 		return -1;
 	}
-	struct uthread_tcb *temp = uthread_current();
-	queue_enqueue(sem->waiting_queue, temp);
-	uthread_block();
-	sem->count--;
+	if (sem->count > 0) {
+		sem->count--;
+	} else {
+		struct uthread_tcb *temp = uthread_current();
+		queue_enqueue(sem->waiting_queue, temp);
+		uthread_block();
+	}
 	return 0;
 }
 
