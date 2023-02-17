d80c07f4cbb5d1d06056045abf78dcff0f169929
GoldyMoon <99521795+GoldyMoon@users.noreply.github.com>
2023-02-17 14:24:17 -0800

    Include private

---
+++ apps/preempt.c
@@ -0,0 +1,89 @@
+#include <signal.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+#include "private.h"
+#include "uthread.h"
+
+#define _XOPEN_SOURCE 700
+
+/*
+ * Frequency of preemption
+ * 100Hz is 100 times per second
+ */
+#define HZ 100
+
+struct sigaction sa,normalsa;
+struct itimerval old, new;
+sigset_t ss;
+
+void signal_handler(int signum) {
+	if(signum == SIGVTALRM){
+		uthread_yield();
+	}
+}
+
+void preempt_disable(void)
+{
+	sigemptyset(&ss);
+	sigaddset(&ss,SIGVTALRM);
+	sigprocmask(SIG_BLOCK, &ss, NULL);
+}
+
+void preempt_enable(void)
+{
+	//Add or not?
+	sigemptyset(&ss);
+	sigaddset(&ss,SIGVTALRM);
+	sigprocmask(SIG_UNBLOCK, &ss, NULL);
+}
+
+void preempt_start(bool preempt)
+{
+	if (!preempt) {
+		return;
+	}
+	//int micro_seconds = 100000/HZ;
+	else{
+		sa.sa_handler = signal_handler;
+		sigemptyset(&sa.sa_mask);
+		sigaddset(&sa.sa_mask, SIGVTALRM);
+		sa.sa_flags = 0;
+		sigaction(SIGVTALRM,&sa,&normalsa);
+
+
+		new.it_interval.tv_usec = 10000;
+		new.it_interval.tv_sec = 0;
+		new.it_value.tv_usec = 10000;
+		new.it_value.tv_sec = 0;
+	
+		int temp = setitimer(ITIMER_VIRTUAL, &new, &old);
+		if (temp < 0) {
+			perror("setitimer fail");
+			exit(1);
+		}
+	}
+}
+
+void preempt_stop(void)
+{
+	//preempt_disable();
+
+	sigaction(SIGVTALRM, &normalsa, NULL);
+
+
+	new.it_interval.tv_usec = 0;
+	new.it_interval.tv_sec = 0;
+	new.it_value.tv_usec = 0;
+	new.it_value.tv_sec = 0;
+	int temp = setitimer(ITIMER_VIRTUAL, &new, &old);
+	if (temp < 0) {
+		perror("setitimer fail");
+		exit(1);
+	}
+}
+
---
+++ apps/queue.c
@@ -0,0 +1,136 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "queue.h"
+
+struct node {
+	void *data;
+	struct node* next;
+};
+
+struct queue {
+	int size;
+	struct node *head;
+	struct node *tail;
+};
+
+queue_t queue_create(void)
+{
+	queue_t queue = (queue_t)malloc(sizeof(struct queue));
+	if (queue == NULL) {
+		return NULL;
+	}
+	queue->size = 0;
+	queue->head = NULL;
+	queue->tail = NULL;
+	return queue;
+}
+
+int queue_destroy(queue_t queue)
+{
+	if (queue == NULL || queue->size != 0) {
+		return -1;
+	}
+	free(queue);
+	return 0;
+}
+
+int queue_enqueue(queue_t queue, void *data)
+{
+	struct node* node = (struct node*)malloc(sizeof(struct node));
+	if (data == NULL || queue == NULL) {
+		return -1;
+	}
+	if (node == NULL) {
+		return -1;
+	}
+	node->data = data;
+	node->next = NULL;
+	if (queue->size == 0) {
+		queue->size++;
+		queue->head = queue->tail = node;
+	} else {
+		queue->size++;
+		queue->tail->next = node;
+		queue->tail = node;
+	}
+	return 0;
+}
+
+int queue_dequeue(queue_t queue, void **data)
+{
+	if (queue == NULL || data == NULL || queue->size == 0) {
+		return -1;
+	}
+	struct node* temp = queue->head;
+	queue->size--;
+	*data = queue->head->data;
+	queue->head = queue->head->next;
+	if (queue->size == 0) {
+		queue->tail = NULL;
+	}
+	free(temp);
+	return 0;
+}
+
+int queue_delete(queue_t queue, void *data)
+{
+	struct node* head = queue->head;
+	struct node* tail = queue->tail;
+	struct node* temp = head;
+	struct node* match;
+
+	if (queue == NULL || data == NULL) {
+		return -1;
+	}
+	//  if the first element match or there's only 1 node
+	if (head->data == data || queue->size == 1) {
+		queue_dequeue(queue, &data);
+		return 0;
+	}
+	//  Loop through the queue
+	while (temp->data != data) {
+		if (temp->next->data == data) {
+			match = temp->next;
+			temp->next = match->next;
+			if (temp->next == tail) {
+				queue->tail = temp;
+			}
+			free(match);
+			queue->size--;
+		} else {
+			temp = temp->next;
+		}
+	}
+	//  Not found
+	if (temp == NULL) {
+		return -1;
+	}
+	return 0;
+}
+
+int queue_iterate(queue_t queue, queue_func_t func)
+{
+	struct node* temp = queue->head;
+	struct node* next;
+	if (queue == NULL || func == NULL) {
+		return -1;
+	}
+	while (true) {
+		if (temp == NULL) {
+			break;
+		}
+		next = temp->next;
+		func(queue, temp->data);
+		temp = next;
+	}
+	return 0;
+}
+
+int queue_length(queue_t queue)
+{
+	return queue->size;
+}
+
