61eb99df9ac9087d2324304dea62ed2d734eff6f
GoldyMoon <99521795+GoldyMoon@users.noreply.github.com>
2023-02-18 14:56:01 -0800

    preemption test file updated

--- apps/test_preempt.c
+++ apps/test_preempt.c
@@ -4,13 +4,20 @@
 
 #include <uthread.h>
 #include <private.h>
+#include <sem.h>
+#include <time.h>
+
+#include <unistd.h>
 
 void thread2(void *arg)
 {
 	(void)arg;
-  while(1){
-    printf("still here t2\n");
-  }
+	int i = 0;
+	printf("should see me here\n");
+	while(1){
+		i++;
+	}
+	printf("i is now %d\n",i);
 }
 
 void thread1(void *arg)
@@ -18,14 +25,14 @@ void thread1(void *arg)
 	(void)arg;
   printf("thread1 entered\n");
 	uthread_create(thread2, NULL);
-  printf("thread1 entered\n");
+  printf("thread1 continue\n");
 	uthread_yield();
 	printf("thread1 reenterd\n");
 }
 
 int main(void)
 {
-	uthread_run(false, thread1, NULL);
-  printf("main thread\n");
+	uthread_run(true, thread1, NULL);
+  //printf("main thread\n");
 	return 0;
 }
