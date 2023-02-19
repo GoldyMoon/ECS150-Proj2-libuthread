a54cb59101708429d487820d8ada4a9a74f913e9
GoldyMoon <99521795+GoldyMoon@users.noreply.github.com>
2023-02-19 12:56:14 -0800

    upload atp

--- apps/test_preempt.c
+++ apps/test_preempt.c
@@ -9,30 +9,59 @@
 
 #include <unistd.h>
 
+int cost = 1;
+int *argsh = &cost;
+
 void thread2(void *arg)
-{
-	(void)arg;
-	int i = 0;
-	printf("should see me here\n");
+{	
+	//int i = 0;
+	int *int_ptr = (int *)arg;
+	printf("thread2 entered\n");
+	//printf("cost is now %d\n",*int_ptr);
+	//uthread_yield();
+	//printf("thread2 reentered\n");
+	printf("cost is now %d\n",*int_ptr);
 	while(1){
+		//printf("cost is now %d\n",cost);
+		/*
+		if(i ==40000){
+			uthread_yield();
+			printf("cost is now %d\n",*int_ptr);
+		}
 		i++;
+		*/
+		preempt_disable();
+		if(*int_ptr == 0){
+			printf("Finish all the loop, about to exit\n");
+			uthread_exit();
+		}
+		preempt_enable();
 	}
-	printf("i is now %d\n",i);
+	
 }
 
 void thread1(void *arg)
 {
 	(void)arg;
-  	printf("thread1 entered\n");
-	uthread_create(thread2, NULL);
-  	printf("thread1 continue\n");
+  printf("thread1 entered\n");
+	uthread_create(thread2, argsh);
+  printf("thread1 continue\n");
 	uthread_yield();
+	preempt_disable();
+	cost = 0;
+	//printf("cost right after change is %d\n",cost);
 	printf("thread1 reenterd\n");
+	printf("cost right after change is %d\n",cost);
+	preempt_enable();
+	//uthread_exit();
 }
 
 int main(void)
 {
 	uthread_run(true, thread1, NULL);
-  //printf("main thread\n");
+  printf("main thread\n");
 	return 0;
 }
+
+//expected output with preempt enable is:
+//thread1 entered, thread1 countinue, should me see me here, thread1 reentered, finish all the loop, about tho exit, main thread
